7507c513d746e0fe83c3350489488bc55ab4fa56
Cezary Kochański <cezarykochanski@MacBook-Air-Cezary.local>
2023-07-19 17:38:47 +0200

    Initial commit

---
+++ src/pl/sda/Main.java
@@ -0,0 +1,12 @@
+package pl.sda;
+
+import java.util.stream.IntStream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Printing numbers from 1 to 10");
+        IntStream.range(1, 10).forEach(System.out::println);
+    }
+
+}
