5c484b1f4bb6e42625398ad84499f25a90603795
Cezary Kochański <cezarykochanski@MacBook-Air-Cezary.local>
2023-07-20 18:06:38 +0200

    fix(main): Fix for range closed numbers printing

--- src/pl/sda/Main.java
+++ src/pl/sda/Main.java
@@ -6,7 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Printing numbers from 1 to 10");
-        IntStream.range(1, 10).forEach(System.out::println);
+        IntStream.rangeClosed(1, 10).forEach(System.out::println);
     }
+
+    /*
+    MAIN-1 - fix the bug
+     */
 
 }
